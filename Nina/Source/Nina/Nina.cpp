fc0714740822f81d97521f795445d95bbeabacf4
sugen.sg <sugen.sg@alibaba-inc.com>
2025-07-21 14:42:09 +0800

    init

---
+++ Nina/Source/Nina/Nina.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nina.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nina, "Nina" );
+
+DEFINE_LOG_CATEGORY(LogNina)
+ 
---
+++ Nina/Source/Nina/NinaGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NinaGameMode.h"
+
+ANinaGameMode::ANinaGameMode()
+{
+	// stub
+}
